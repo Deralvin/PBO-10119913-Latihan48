dc66d5105073291c4e8296a109e630320197e27e
Dilan <ramadhanrizqi21@gmail.com>
2020-11-15 09:58:38 +0700

    Latihan 48

---
+++ src/pbo10119913latihan48/Pbo10119913Latihan48.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo10119913latihan48;
+
+/**
+/**
+ * @author Muhammad Alvin Rizqi Ramadhan
+ * Kelas : IF10-K
+ * NIM :10119913 
+ */
+public class Pbo10119913Latihan48 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Kalkulator hitung = new Kalkulator();
+        
+        System.err.println("value1 = 7.0 ");
+        hitung.setValue1(7.0);
+        System.out.println("value2 = 5.0");
+        hitung.setValue2(5.0);
+        System.out.println("");
+        hitung.setNamaProject();
+        System.out.println("");
+        hitung.setNoteProject("This project shown you how to manage method in java");
+        System.out.println("Result add is = " + hitung.add(7.0, 5.0));
+        System.out.println("Result minus is = " + hitung.minus(7.0, 5.0));
+        System.out.println("Result multiple is = " + hitung.minus(7.0, 5.0));
+        System.out.println("Result division id = " + hitung.division(7.0,5.0));
+    }
+    
+}
